59e56c4988bc0464fa930f0e5a7972b8ead1455f
Jay Faulkner <jayleefaulkner@gmail.com>
2020-05-17 16:55:44 +0100

    Initial (real) commit

---
+++ AudicaModStarter/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using MelonLoader;
+using AudicaModding;
+
+[assembly: AssemblyTitle(MyMod.BuildInfo.Name)]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(MyMod.BuildInfo.Company)]
+[assembly: AssemblyProduct(MyMod.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + MyMod.BuildInfo.Author)]
+[assembly: AssemblyTrademark(MyMod.BuildInfo.Company)]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+//[assembly: Guid("")]
+[assembly: AssemblyVersion(MyMod.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(MyMod.BuildInfo.Version)]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: MelonModInfo(typeof(MyMod), MyMod.BuildInfo.Name, MyMod.BuildInfo.Version, MyMod.BuildInfo.Author, MyMod.BuildInfo.DownloadLink)]
+
+
+// Create and Setup a MelonModGame to mark a Mod as Universal or Compatible with specific Games.
+// If no MelonModGameAttribute is found or any of the Values for any MelonModGame on the Mod is null or empty it will be assumed the Mod is Universal.
+// Values for MelonModGame can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
+[assembly: MelonModGame(null, null)]
---
+++ AudicaModStarter/src/Main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using MelonLoader;
+using UnityEngine;
+
+namespace AudicaModding
+{
+    public class MyMod : MelonMod
+    {
+        public static class BuildInfo
+        {
+            public const string Name = "AudicaModStarter";  // Name of the Mod.  (MUST BE SET)
+            public const string Author = "AudicaModAuthor"; // Author of the Mod.  (Set as null if none)
+            public const string Company = null; // Company that made the Mod.  (Set as null if none)
+            public const string Version = "1.0.0"; // Version of the Mod.  (MUST BE SET)
+            public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
+        }
+        public override void OnUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+                MelonModLogger.Log("You just pressed T");
+            }
+        }
+    }
+}
