044062166f115d6e0fc4421352154b63a82585ad
Jay Faulkner <jayleefaulkner@gmail.com>
2020-05-17 19:12:21 +0100

    chore: remove reference binaries, pull in via script instead

---
+++ copy-references.ps1
@@ -0,0 +1,46 @@
+$AudicaFolder=$args[0]
+$ModFolder=$args[1] 
+
+$ReferenceFiles = "Assembly-CSharp.dll", "Assembly-CSharp-firstpass.dll", "Il2CppMono.Security.dll", "Il2Cppmscorlib.dll", "Il2CppSystem.Configuration.dll", "Il2CppSystem.Core.dll", "Il2CppSystem.dll", "Il2CppSystem.Runtime.Serialization.dll", "Il2CppSystem.Xml.dll", "Oculus.VR.dll", "Sanford.Multimedia.Midi.dll", "SonyNP.dll", "SteamVR.dll", "TwitchChatter.dll", "UnhollowerBaseLib.dll", "UnhollowerRuntimeLib.dll", "Unity.TextMeshPro.dll", "UnityEngine.AIModule.dll", "UnityEngine.AnimationModule.dll", "UnityEngine.AudioModule.dll", "UnityEngine.CoreModule.dll", "UnityEngine.IMGUIModule.dll", "UnityEngine.ParticleSystemModule.dll", "UnityEngine.Physics2DModule.dll", "UnityEngine.PhysicsModule.dll", "UnityEngine.TerrainModule.dll", "UnityEngine.TextCoreModule.dll", "UnityEngine.TextRenderingModule.dll", "UnityEngine.UI.dll", "UnityEngine.UIModule.dll", "UnityEngine.UnityWebRequestModule.dll", "UnityEngine.UnityWebRequestWWWModule.dll", "UnityEngine.VehiclesModule.dll", "UnityEngine.XRModule.dll"
+
+if ([string]::IsNullOrWhiteSpace($AudicaFolder) -Or [string]::IsNullOrWhiteSpace($ModFolder)) { 
+    Write-Host "Copys extracted melon loader references from audica folder to mod project references."
+    Write-Host "Usage:"
+    Write-Host ".\copy-references.ps1 path\to\audica\folder name-of-mod-folder"
+}
+
+
+# -- CHECK THIS IS ACTUAL AUDICA FOLDER --
+if (!(Test-Path -Path "${AudicaFolder}\Audica.exe")) {
+    Write-Host -ForegroundColor red "Provided path does not exist or is not the audica folder!"
+    exit
+}
+
+# -- CHECK FOR MELON MOD --
+if (!(Test-Path -Path "${AudicaFolder}\MelonLoader\Managed\") -Or !(Test-Path -Path "${AudicaFolder}\winmm.dll")) {
+    Write-Host -ForegroundColor red "MelonLoader has not been installed in the Audica folder! See the README."
+    exit
+}
+
+# -- CHECK FOR MOD FOLDER --
+# absolute check first
+if (!(Test-Path -Path "${ModFolder}")) {
+
+    # if that failed, try a relative check
+    ${ModFolder} = "${PSScriptRoot}\${ModFolder}"
+    if (!(Test-Path -Path "${ModFolder}")) {
+        Write-Host -ForegroundColor red "Mod project folder ${PSScriptRoot}\${ModFolder} does not exist"
+        exit
+    }
+}
+
+# -- COPY FILES --
+
+Write-Host -ForegroundColor green "Checks passed! Copying files ..."
+
+Write-Host "Copying MelonLoader.ModHandler.dll ..."
+Copy-Item "${AudicaFolder}\MelonLoader\MelonLoader.ModHandler.dll" -Destination "${ModFolder}\Lib"
+foreach ($file in $ReferenceFiles) {
+    Write-Host "Copying ${file} ..."
+    Copy-Item "${AudicaFolder}\MelonLoader\Managed\${file}" -Destination "${ModFolder}\Lib"
+}
